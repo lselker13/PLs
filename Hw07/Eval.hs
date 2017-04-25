d068746ddb635d213c9690b3f045646e83629d8f
Madhura Jayaraman <m.jayaraman2@gmail.com>
2017-04-24 21:23:42 -0700

    finished subst

--- Hw07/Eval.hs
+++ Hw07/Eval.hs
@@ -15,15 +15,37 @@ import System.IO
 
 import Parse
 
+
+
+--data LExp =
+--    | LetRec VarName Type LExp LExp
+
 --let x = e1 in e2
 --e2 -> x -> e1 
 subst :: LExp -> VarName -> LExp -> LExp
 subst (Var x) y e | x == y    = e
                   | otherwise = Var x
-subst (Ap e1 e2) y e         = Ap (subst e1 y e) (subst e2 y e)
-subst (Lambda x t e1) y e | x == y = Lambda x t e1
-                        | otherwise = Lambda x t (subst e1 y e)
-subst (Num n) _ _ = (Num n)
+subst (Ap e1 e2) y e = Ap (subst e1 y e) (subst e2 y e)
+subst (Lambda x t e1) y e | x == y    = Lambda x t e1
+                          | otherwise = Lambda x t (subst e1 y e)
+subst e@(Num _) _ _ = e
+subst e@(Bool _) _ _ = e
+subst (If e1 e2 e3) y e = If (subst e1 y e) (subst e2 y e) (subst e3 y e)
+subst (Pair e1 e2) y e = Pair (subst e1 y e) (subst e2 y e)
+subst (Plus e1 e2) y e = Plus (subst e1 y e) (subst e2 y e)
+subst (Minus e1 e2) y e = Minus (subst e1 y e) (subst e2 y e)
+subst (Times e1 e2) y e = Times (subst e1 y e) (subst e2 y e)
+subst (Divide e1 e2) y e = Divide (subst e1 y e) (subst e2 y e)
+subst (And e1 e2) y e = And (subst e1 y e) (subst e2 y e)
+subst (Or e1 e2) y e = Or (subst e1 y e) (subst e2 y e)
+subst (Lt e1 e2) y e = Lt (subst e1 y e) (subst e2 y e)
+subst (Eqq e1 e2) y e = Eqq (subst e1 y e) (subst e2 y e)
+subst (Neg e1) y e = Neg $ subst e1 y e
+subst (Not e1) y e = Not $ subst e1 y e
+subst (Fst e1) y e = Fst $ subst e1 y e
+subst (Snd e1) y e = Snd $ subst e1 y e
+subst (Typed e1 t) y e = subst e1 y e
+subst (Let x e1 e2) y e = subst (subst e2 x e1) y e
 
 {-
 data LExp =
